ac5609cd776d62c8c60188c9e39f2bf533860ff0
Tom Bradford <tommbradford@hotmail.co.uk>
2015-10-30 22:43:28 +0000

    Added config menu
    Added the initialisation menu at the start of program to know what mode
    the Card Reader is in.

--- RailSmartCard/src/CardReader.java
+++ RailSmartCard/src/CardReader.java
@@ -4,18 +4,54 @@ public class CardReader
 {
 	final private static String DATA_FILE = "D:/Eclipse/RailSmartCard/tmp/data";
 	
+	private int _MODE_ = 0;	
+	private String _STATION_NAME_;
+	private int _SERVICE_ID_;
+	
 	CardReader()
 	{
 		
 	}
 	
-	public Boolean doesIDFileExist()
+	CardReader(int mode, String stationName)
+	{
+		_MODE_ = mode;
+		_STATION_NAME_ = stationName;
+		_SERVICE_ID_ = 0;
+	}
+	
+	CardReader(int mode, int serviceID)
+	{
+		_MODE_ = mode;
+		_STATION_NAME_ = null;
+		_SERVICE_ID_ = serviceID;
+	}
+		
+	private String getMode()
+	{
+		if(_MODE_ == 1)
+		{
+			return "STATION";
+		}
+		return "CONDUCTOR";
+	}
+	
+	private String getWelcomeMessage()
+	{
+		if(_MODE_ == 1)
+		{
+			return "THIS TERMINAL IS RUNNING AT " + _STATION_NAME_.toUpperCase() + " STATION";
+		}
+		return "THIS TERMINAL IS OPERATING ON SERVICE ID: " + _SERVICE_ID_;
+	}
+
+	private Boolean doesIDFileExist()
 	{
 		File file = new File(DATA_FILE);
 		return file.exists();
 	}
 	
-	public String readIDFile() 
+	private String readIDFile() 
 	{
 		String data = null;
 		FileReader fReader = null;
@@ -33,7 +69,7 @@ public class CardReader
 		return data;
 	}
 	
-	public String retrieveCardID(String data)
+	private String retrieveCardID(String data)
 	{
 		int start = data.indexOf("Text");
 		int end = data.indexOf("EncodingType");
@@ -48,14 +84,14 @@ public class CardReader
 		}
 	}
 	
-	public void deleteFile()
+	private void deleteFile()
 	{
 		File toBeDeleted = new File(DATA_FILE);
 		toBeDeleted.delete();
 		systemPause();
 	}
 	
-	public void systemPause()
+	private void systemPause()
 	{
 		try
 		{
@@ -66,7 +102,31 @@ public class CardReader
 			e.printStackTrace();
 		}
 	}
-
-
- 
+	
+	public void run()
+	{
+		System.out.println("*** SYSTEM RUNNING IN " + getMode() + " MODE ***");
+		System.out.println("*** " + getWelcomeMessage() +" ***\n");
+		Boolean isMessageDisplayed = false;
+		while(true)	
+		{	
+			if(!isMessageDisplayed)
+			{
+				System.out.println("Please present card");
+				isMessageDisplayed = true;
+			}
+			systemPause();			
+			if (doesIDFileExist())
+			{			
+				String ID = retrieveCardID(readIDFile());			
+				if (ID != "-1")
+				{
+					System.out.println("ID = " + ID);				
+					isMessageDisplayed = false;
+					System.out.println("");
+				}
+				deleteFile();	
+			}
+		}		
+	}
 }
--- RailSmartCard/src/main.java
+++ RailSmartCard/src/main.java
@@ -1,175 +1,30 @@
-
-import java.io.*;
 import java.util.Scanner;
 
 public class main 
 {
-	final static String DATA_FILE = "D:/Eclipse/RailSmartCard/tmp/data";
-	final static String STATIONS_FILE = "D:/Eclipse/RailSmartCard/bin/Stations.txt";
-	
 	public static void main(String[] args)
 	{
-		CardReader cReader = new CardReader();
-		Boolean isMessageDisplayed = false;
+		CardReader cReader;
 		
-		while(true)	
-		{
-			if(!isMessageDisplayed)
-			{
-				System.out.println("Please present card");
-				isMessageDisplayed = true;
-			}
-			cReader.systemPause();			
-			if (cReader.doesIDFileExist())
-			{			
-				String ID = cReader.retrieveCardID(cReader.readIDFile());			
-				if (ID != "-1")
-				{
-					System.out.println("ID = " + ID);				
-					isMessageDisplayed = false;
-					System.out.println("");
-				}
-				cReader.deleteFile();	
-			}
-		}
-	}
-	
-	private static void displayMainMenu()
-	{
-		System.out.println("Welcome to Rail Smartcard System");
-		System.out.println("Please select which mode to run system in:");
-		System.out.println("1 - Station");
-		System.out.println("2 - Conductor");
-		System.out.println("0 - Quit");
-	}
-	
-	private static int selectOperatingMode()
-	{
-		int choice = -1;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter operating mode (1:Station, 2:Conductor): ");
+		int mode = scanner.nextInt();
+		int serviceID = 0;
+		String stationName = null;
 		
-		Scanner scanner = null;
-		try
+		if(mode == 1)
 		{
-			scanner = new Scanner(System.in);
-			
-			while(choice > 2 || choice < 0)
-			{	
-				System.out.print("\nOption: ");
-				choice = scanner.nextInt();
-			}
+			System.out.print("Enter station name: ");
+			stationName = scanner.next();
+			cReader = new CardReader(mode, stationName);
 		}
-		finally
+		else
 		{
-			scanner.close();
+			System.out.print("Enter service ID: ");
+			serviceID = scanner.nextInt();
+			cReader = new CardReader(mode, serviceID);
 		}
-		
-		return choice;
-	}
-	
-	private static void displayStationMenu()
-	{
-		System.out.println("Please insert the station code (or 0 to return to main menu)");
-		System.out.println("Station code: ");
-		selectStationCode();
-	}
-	
-	private static String selectStationCode()
-	{
-		int choice = -1;
-		
-		Scanner scanner = null;
-		try
-		{
-			scanner = new Scanner(System.in);
-			
-			while(choice > 2 || choice < 0)
-			{	
-				System.out.print("\nOption: ");
-				choice = scanner.nextInt();
-			}
-		}
-		finally
-		{
-			scanner.close();
-		}
-		
-		return "";
-	}
-	
-	private static String searchStationFile()
-	{
-		String data = null;
-		FileReader fReader = null;
-		try
-		{
-			fReader = new FileReader(STATIONS_FILE);
-			BufferedReader reader = new BufferedReader(fReader);	
-			data = reader.readLine();
-			fReader.close();
-		} 
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		return "";
-	}
-	
-	
-// 	private static Boolean doesIDFileExist()
-//	{
-//		File file = new File(DATA_FILE);
-//		return file.exists();
-//	}
-//	
-//	private static String readIDFile() 
-//	{
-//		String data = null;
-//		FileReader fReader = null;
-//		try
-//		{
-//			fReader = new FileReader(DATA_FILE);
-//			BufferedReader reader = new BufferedReader(fReader);	
-//			data = reader.readLine();
-//			fReader.close();
-//		} 
-//		catch (IOException e)
-//		{
-//			e.printStackTrace();
-//		}
-//		return data;
-//	}
-//	
-//	private static String retrieveCardID(String data)
-//	{
-//		int start = data.indexOf("Text");
-//		int end = data.indexOf("EncodingType");
-//		
-//		if((start != -1) || (end != -1))
-//		{
-//			return data.substring(start+7, end-3);
-//		}
-//		else
-//		{
-//			return "-1";
-//		}
-//	}
-//	
-//	private static void deleteFile()
-//	{
-//		File toBeDeleted = new File(DATA_FILE);
-//		toBeDeleted.delete();
-//		waitOneSecond();
-//	}
-//	
-//	private static void waitOneSecond()
-//	{
-//		try
-//		{
-//			Thread.sleep(1000);
-//		}
-//		catch (Exception e)
-//		{
-//			e.printStackTrace();
-//		}
-//	}
+		scanner.close();
+		cReader.run();
+	}	
 }
